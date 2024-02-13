30e452e2001708f2c85492a25772cc4db9e6d179
Jacky HE <ihkk.nb@gmail.com>
2024-02-13 23:32:55 +0000

    change url in homepage

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function App() {
     <div className="App">
       <div className="container">
         <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a href="https://ihkk.github.io/Web-ASSReader/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+          <a href="https://ass.h0.work/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
             <span class="fs-4">ASS Reader</span>
           </a>
 
