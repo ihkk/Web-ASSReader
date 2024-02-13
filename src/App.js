c70d80809cfbb2c7de3325f60a7c0afa2d021549
Jacky HE <ihkk.nb@gmail.com>
2024-02-13 17:36:15 +0000

    hide the columns if not needed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './components/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import assParser from 'ass-parser';
@@ -31,6 +30,15 @@ function App() {
     }
   };
 
+  // check if to display Actor/Name
+  const shouldShowNameColumn = events.some(event => event.value.Name.trim() !== '');
+  // check if to display Effect
+  const shouldShowEffectColumn = events.some(event => event.value.Effect.trim() !== '');
+  // Margins
+  const shouldShowMarginLColumn = events.some(event => event.value.MarginL.trim() !== 0);
+  const shouldShowMarginRColumn = events.some(event => event.value.MarginR.trim() !== 0);
+  const shouldShowMarginVColumn = events.some(event => event.value.MarginV.trim() !== 0);
+
   return (
     <div className="App">
       <div className="container">
@@ -58,29 +66,29 @@ function App() {
             <table className="table">
               <thead>
                 <tr>
-                  <th>Start</th>
-                  <th>End</th>
+                  <th style={{ minWidth: '100px' }}>Start</th>
+                  <th style={{ minWidth: '100px' }}> End</th>
                   <th>Style</th>
-                  <th>Name</th>
-                  <th>L</th>
-                  <th>R</th>
-                  <th>V</th>
-                  <th>Effect</th>
+                  {shouldShowNameColumn && <th>Actor</th>}
+                  {shouldShowMarginLColumn && <th style={{ minWidth: '50px' }}>L</th>}
+                  {shouldShowMarginRColumn && <th style={{ minWidth: '50px' }}>R</th>}
+                  {shouldShowMarginVColumn && <th style={{ minWidth: '20px' }}>V</th>}
+                  {shouldShowEffectColumn && <th>Effect</th>}
                   <th>Text</th>
 
                 </tr>
               </thead>
               <tbody>
                 {events.map((event, index) => (
-                  <tr key={index}>
+                  <tr key={index} className={event.key === "Comment" ? "comment" : ""}>
                     <td>{event.value.Start}</td>
                     <td>{event.value.End}</td>
                     <td>{event.value.Style}</td>
-                    <td>{event.value.Name}</td>
-                    <td>{event.value.MarginL}</td>
-                    <td>{event.value.MarginR}</td>
-                    <td>{event.value.MarginV}</td>
-                    <td>{event.value.Effect}</td>
+                    {shouldShowNameColumn && <td>{event.value.Name}</td>}
+                    {shouldShowMarginLColumn && <td>{event.value.MarginL}</td>}
+                    {shouldShowMarginRColumn && <td>{event.value.MarginR}</td>}
+                    {shouldShowMarginVColumn && <td>{event.value.MarginV}</td>}
+                    {shouldShowEffectColumn && <td>{event.value.Effect}</td>}
                     <td>{event.value.Text}</td>
                   </tr>
                 ))}
@@ -107,7 +115,7 @@ function App() {
 
 
       </div>
-    </div>
+    </div >
   );
 }
 
