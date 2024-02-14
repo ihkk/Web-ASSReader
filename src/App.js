dff3317b3343022835d150ce6afebf1457868ee4
Jacky HE <ihkk.nb@gmail.com>
2024-02-14 00:01:49 +0000

    change to container-fluid

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className="container">
+      <div className="container-fluid">
         <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
           <a href="https://ass.h0.work/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
             <span class="fs-4">ASS Reader</span>
